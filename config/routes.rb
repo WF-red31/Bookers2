0b70e36d19ed897b020707fc3d0727d0e6c26a87
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-1.compute.internal>
2024-06-21 08:19:03 +0000

    [Finish]投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/rails'
-  get 'books/g'
-  get 'books/controller'
-  get 'books/books'
   root to: 'homes#top'
   devise_for :users
 
   resources :users, only: [:show, :index, :edit]
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
