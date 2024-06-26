0c7ee30e07556732afd9eb27326f7a2724fba875
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-1.compute.internal>
2024-06-26 06:09:52 +0000

    ユーザーの編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
